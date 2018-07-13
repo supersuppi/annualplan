9dd3cac190a6c98dc07c7003b0aebfcfc6cb55ec
suprith <suprith.me@gmail.com>
2018-07-13 20:14:43 +0530

    intial commit-Plain angular and spring boot app

---
+++ ap-server/src/main/java/com/gxh/apserver/ApServerApplication.java
@@ -0,0 +1,12 @@
+package com.gxh.apserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApServerApplication.class, args);
+	}
+}
