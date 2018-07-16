b9a109b1997c12d332dabf947fc873da84f621b3
suprith <suprith.me@gmail.com>
2018-07-16 21:10:38 +0530

    base package created

--- ap-server/src/main/java/com/gxh/apserver/ApServerApplication.java
+++ ap-server/src/main/java/com/gxh/apserver/ApServerApplication.java
@@ -2,11 +2,20 @@ package com.gxh.apserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ApServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApServerApplication.class, args);
 	}
+
+	@RequestMapping("/hello")
+	String home() {
+		return "Hello World!";
+	}
+
 }
