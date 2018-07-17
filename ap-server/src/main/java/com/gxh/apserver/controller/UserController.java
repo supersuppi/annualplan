9b7cb5df255b6fd8abaad45e8916973086b339b3
Vishwas <M1028002@mindtree.com>
2018-07-17 17:42:20 +0530

    Removing roleservice and controller

--- ap-server/src/main/java/com/gxh/apserver/controller/UserController.java
+++ ap-server/src/main/java/com/gxh/apserver/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.gxh.apserver.controller;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gxh.apserver.entity.Role;
+import com.gxh.apserver.service.UserService;
+
+@RestController
+@RequestMapping(value="/user")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/role", method=RequestMethod.GET)
+	public ResponseEntity<Collection<Role>> getUserRoles(){
+		
+	 return ResponseEntity.status(HttpStatus.ACCEPTED).header("content-type", "application/json")
+				.body(userService.getAllRoles());
+	}
 }
--- ap-server/src/main/java/com/gxh/apserver/service/UserService.java
+++ ap-server/src/main/java/com/gxh/apserver/service/UserService.java
@@ -1,4 +1,19 @@
 package com.gxh.apserver.service;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gxh.apserver.entity.Role;
+import com.gxh.apserver.repository.interfaces.RolesRepository;
+
 public class UserService {
+	
+	@Autowired
+	private RolesRepository rolesRepository;
+	
+	public Collection<Role> getAllRoles() {
+		return rolesRepository.findAll();
+	}
+	
 }
