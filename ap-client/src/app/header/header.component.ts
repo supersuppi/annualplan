f805f01ce57b4bd287c8ae608f6943ab81331ecc
Vishwas <M1028002@mindtree.com>
2018-07-20 16:03:21 +0530

    Adding navigation to login page on click of logout

--- ap-client/src/app/header/header.component.ts
+++ ap-client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
 
   onLogout() {
     console.log("Clicked on Logout");
+    localStorage.clear();
+    this.router.navigate(['\login']);
   }
 
   onShowProfile() {
