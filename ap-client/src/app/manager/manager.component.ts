5c44a20395dd8c83fdb9e58ad263edc7810207c0
suprith <suprith.me@gmail.com>
2018-07-30 22:24:13 +0530

    Manager component updated

--- ap-client/src/app/manager/manager.component.ts
+++ ap-client/src/app/manager/manager.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Promotion } from "../models/index";
+import {PromotionService} from '../services/index'
+
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
-  styleUrls: ['./manager.component.scss']
+  styleUrls: ['./manager.component.scss'],
+  providers: [PromotionService]
 })
 export class ManagerComponent implements OnInit {
 
-  constructor() { }
+  promotion:Promotion;
 
-  ngOnInit() { }
+  constructor(private promotionService:PromotionService) { }
+
+  ngOnInit() {
+    this.getSupplierPromotion(1,'2018-02-02');
+   }
+
+   getSupplierPromotion(id:Number,promoyear:String) {
+    this.promotionService.getSupplierPromotionsForManager(id,promoyear).subscribe((sPromotion:Promotion) => {
+        console.debug("Get SupplierPromotion Call Success");
+        this.promotion = sPromotion;
+      },
+      error => { 
+          console.error("ERROR! ManagerComponent:getSupplierPromotion = "+error);
+      });
+    }
+
+    acceptPromotion() {
+      console.debug("acceptPromotion");
+    }
+
+    rejectPromotion() {
+      console.debug("rejectPromotion");
+    }
+
+   
 
 }
--- ap-client/src/app/services/promotion.service.ts
+++ ap-client/src/app/services/promotion.service.ts
@@ -7,20 +7,27 @@ import { Promotion } from "../models/index";
 @Injectable()
 export class PromotionService{
 
-    private promotionSupplierGetUrl = "http://localhost:8008/apserver/promotion/supplier/";
-    private promotionSupplierSaveUrl = "http://localhost:8008/apserver/promotion/save/";
+    private promotionSupplierGetURL = "http://localhost:8008/apserver/promotion/supplier/";
+    private promotionSupplierSaveURL = "http://localhost:8008/apserver/promotion/save/";
+    private promotionSupplierForManagerURL = "http://localhost:8008/apserver/promotion/manager/";
 
     constructor(private httpClient: HttpClient) {}
 
     getSupplierPromotions(supplierID:Number,promoYear:String): Observable<Promotion>{
-        return this.httpClient.get<Promotion>(this.promotionSupplierGetUrl+supplierID+"/"+promoYear, {
+        return this.httpClient.get<Promotion>(this.promotionSupplierGetURL+supplierID+"/"+promoYear, {
             responseType: 'json'
         });
     }
 
     saveSupplierPromotions(data:Promotion): Observable<Object>{
-        return this.httpClient.post<Promotion>(this.promotionSupplierSaveUrl,data, {
+        return this.httpClient.post<Promotion>(this.promotionSupplierSaveURL,data, {
             headers: new HttpHeaders().set('Content-Type', 'application/json')
+        });
+    }
+
+    getSupplierPromotionsForManager(supplierID:Number,promoYear:String): Observable<Promotion>{
+        return this.httpClient.get<Promotion>(this.promotionSupplierForManagerURL+supplierID+"/"+promoYear, {
+            responseType: 'json'
         });
     }
 }
