5b083cdeca55832c650a0b715e96557d9dcb31e7
suprith <suprith.me@gmail.com>
2018-08-25 19:04:33 +0530

    server : configured to be able to deploy in tomcat

--- ap-server/src/main/java/com/gxh/apserver/ApServerApplication.java
+++ ap-server/src/main/java/com/gxh/apserver/ApServerApplication.java
@@ -1,13 +1,25 @@
 package com.gxh.apserver;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class ApServerApplication {
+public class ApServerApplication extends SpringBootServletInitializer {
+
+	//This is required for standalone app, to deploy in tomcat
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApServerApplication.class, args);
@@ -18,7 +30,7 @@ public class ApServerApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	String home() {
 		return "Hello World!";
 	}
